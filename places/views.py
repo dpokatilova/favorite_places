fa39a9227434d65e5d26ef7c950bd771cc707a1e
Daria Pokatilova <dasha.pokatilova@gmail.com>
2026-09-13 19:46:04 +0300

    Created a home_view function with random choice of location

--- places/views.py
+++ places/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+import random
+from .data import FAVORITE_PLACES
 
-# Create your views here.
+def home_view(request):
+    featured_place = None
+    if FAVORITE_PLACES:
+        weights = []
+        for place in FAVORITE_PLACES:
+            rating = place.get("rating", 1)
+            weights.append(rating)
+
+        featured_place = random.choices(FAVORITE_PLACES, weights=weights, k=1)[0]
+    context = {
+        "featured_place": featured_place
+    }
+    return render(request, "places/home.html", context)
