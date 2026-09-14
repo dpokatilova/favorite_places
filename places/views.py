57a786592a89c82393d33e512220f7bea44f3964
Daria Pokatilova <dasha.pokatilova@gmail.com>
2026-09-14 16:31:48 +0300

    Add add_place_view function to check GET and POST requests with form validation

--- places/views.py
+++ places/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 import random
 from .data import FAVORITE_PLACES
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .forms import NewPlaceForm
 
 def home_view(request):
     featured_place = None
@@ -35,3 +38,24 @@ def place_detail_view(request, place_id):
         "place": found_place
     }
     return render(request, "places/place_detail.html", context)
+
+
+def add_place_view(request):
+    if request.method == 'POST':
+        form = NewPlaceForm(request.POST)
+        if form.is_valid():
+            new_place = {
+                "id": len(FAVORITE_PLACES) + 1,
+                "title": form.cleaned_data['title'],
+                "type": form.cleaned_data['place_type'],
+                "location": form.cleaned_data['location'],
+                "rating": form.cleaned_data['rating'],
+                "description": form.cleaned_data['description'],
+                "date_added": "2026-09-14",
+            }
+            FAVORITE_PLACES.append(new_place)
+            return HttpResponseRedirect(reverse('places_list'))
+    else:
+        form = NewPlaceForm()
+
+    return render(request, 'places/add_place.html', {'form': form})
