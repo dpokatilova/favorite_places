b19373f71885c24fa42b406d03e46b9ec5133224
Daria Pokatilova <dasha.pokatilova@gmail.com>
2026-09-14 14:10:51 +0300

    Fix place_detail url and add int parametr

--- places/urls.py
+++ places/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home_view, name= "home"),
     path("places/", views.places_list_view, name="places_list"),
-    path("places//", views.place_detail_view, name= "places_detail")
+    path("places/<int:place_id>/", views.place_detail_view, name="place_detail")
 ]
