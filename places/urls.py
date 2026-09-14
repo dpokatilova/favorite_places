a89a103233421b334febe729b23afa01cc611fdd
Daria Pokatilova <dasha.pokatilova@gmail.com>
2026-09-14 16:35:52 +0300

    Add url for adding a new place

--- places/urls.py
+++ places/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home_view, name= "home"),
     path("places/", views.places_list_view, name="places_list"),
-    path("places/<int:place_id>/", views.place_detail_view, name="place_detail")
+    path("places/<int:place_id>/", views.place_detail_view, name="place_detail"),
+path('add/', views.add_place_view, name='add_place')
 ]
